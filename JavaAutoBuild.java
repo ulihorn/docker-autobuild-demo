74c9fd209cfe1a09ed43bd524d132ca054ce3532
Ulrich Hoelscher <ulrich.hoelscher@emc.com>
2016-04-08 22:06:19 +0200

    Commit the Java Test Application

---
+++ JavaAutoBuild.java
@@ -0,0 +1,7 @@
+public class JavaAutoBuild {
+
+  public static void main(String[] args) {
+    System.out.println("Hallo Docker Workshop!");
+    System.out.println("Ich wurde automatisch erstellt!");
+  }
+}
