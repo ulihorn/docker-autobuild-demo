e517cdcf15a2d5c48e2782d788c487bf7c6f913f
Ulrich Hölscher <ulrich.hoelscher@emc.com>
2016-07-21 00:18:35 +0200

    Docker Donnerstag

--- JavaAutoBuild.java
+++ JavaAutoBuild.java
@@ -2,6 +2,6 @@ public class JavaAutoBuild {
 
   public static void main(String[] args) {
     System.out.println("Hallo Docker Workshop!");
-    System.out.println("Ich wurde automatisch erstellt!");
+    System.out.println("Ich wurde am Docker Donnerstag automatisch erstellt!");
   }
 }
